83e9deffab2cc264114ef5eac7181f19116d8a29
Anug Saha <anugsaha@gmail.com>
2021-06-08 19:23:53 -0400

    memoization practice

--- dynamic_programming/memoization_project/lib/leet_code_518.js
+++ dynamic_programming/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,16 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+var change = function(amount, coins, memo={}) {
+    let key = amount + '' + coins; 
+    if (key in memo) return memo[key]; 
+    if(amount===0) return 1; 
+
+    let used_coin= coins[coins.length - 1]; 
+    total = 0; 
+    for(let i=0; used_coin * i <= amount ;i++){ 
+        total += change(amount- used_coin*i, coins.slice(0,-1), memo); 
+    }
+    memo[key]= total; 
+    return memo[key]; 
+};
