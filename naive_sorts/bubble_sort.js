478edae2871c642827e2a4adf9c7b581c6c5c30a
Anug Saha <anugsaha@gmail.com>
2021-06-14 22:58:41 -0400

    understand naive sorts

---
+++ naive_sorts/bubble_sort.js
@@ -0,0 +1,62 @@
+// The idea behind bubble sort is that every pair of 
+// adjacent values is compared, and then the two values 
+// swap positions if the first value is greater than the
+// second. This way during each pass through the array, the
+// largest value “bubbles up” to the top, and after each 
+// pass the elements furthest to the right are in the correct order.
+
+bubble_sort= (arr) => { 
+    let sorted= false; 
+
+    while(!sorted){ 
+
+        sorted=true; 
+
+        for(let i=0; i< arr.length-1; i++){ 
+            if(arr[i]>arr[i+1]){ 
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]]
+                // let tmp = arr[i];
+                // arr[i] = arr[i + 1];
+                // arr[i + 1] = tmp;
+                sorted = false
+            } 
+        }
+    }
+    return arr
+}
+
+// console.log(bubble_sort([3,1,12,4,6,5,16,15]))
+
+
+//callback method
+
+let larger= (ele1, ele2) => { 
+    if(ele1 > ele2){ 
+        return 1
+    }else if(ele1 < ele2){ 
+        return -1
+    }else{ 
+        return 0
+    }
+} 
+
+Array.prototype.bubble= function (callback = null){ 
+    if(callback=== null) callback= larger; 
+
+    let arr= this.slice(); 
+    let sorted= false; 
+
+    while(!sorted){ 
+        sorted=true; 
+        for(i=0; i< arr.length-1; i++){ 
+            if(callback(arr[i], arr[i+1])=== 1){ 
+                sorted = false; 
+                [arr[i], arr[i+1]]= [arr[i+1], arr[i]]
+            }
+        }
+    } 
+    return arr
+
+}
+
+console.log([3,1,12,4,6,5,16,15].bubble())
---
+++ naive_sorts/selection_sort.js
@@ -0,0 +1,20 @@
+//  Selection sort selects the smallest elements of the array and directly places 
+// them at the beginning of the array in sorted position
+
+selection_sort= (arr)=> { 
+
+    for(let i=0; i< arr.length; i++){ 
+        let minIdx= i; 
+        for(let j=i+1; j< arr.length; j++){ 
+            if(arr[minIdx]> arr[j]){ 
+                minIdx = j; 
+            }
+        }
+        [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]]; 
+    }
+    return arr
+
+
+}
+
+console.log(selection_sort([3,1,12,4,6,5,16,15]))
