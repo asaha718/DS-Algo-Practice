90e8f395f5c4ce6de8dddc32bd52140e93fd6698
Anug Saha <anugsaha@gmail.com>
2021-09-11 20:24:47 -0400

    code quick sort from notes

--- efficent_sorts/quick_sort/quick_sort.js
+++ efficent_sorts/quick_sort/quick_sort.js
@@ -1,2 +1,20 @@
 //Easy to sort elements of an array relative to a particular target value
 
+const quickSort = (array) => {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let pivot = array.shift();
+  let left = array.filter((el) => pivot > el);
+  let right = array.filter((el) => pivot <= el);
+
+  let leftSorted= quickSort(left); 
+  let rightSorted= quickSort(right); 
+
+  return [...leftSorted, pivot, ...rightSorted]
+};
+
+let arr = [42, 95, 53, 75, 12, 8, 24];
+
+console.log(quickSort(arr))
